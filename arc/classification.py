3cfafa6abdbcc0e505adb8292118b32adf7f4334
Matteo Sesia <msesia@stanford.edu>
2020-05-21 11:01:56 -0700

    Fixed bug in nonrandomized decision rule

--- arc/classification.py
+++ arc/classification.py
@@ -12,18 +12,17 @@ class ProbabilityAccumulator:
         self.Z = np.round(self.prob_sort.cumsum(axis=1),9)        
         
     def predict_sets(self, alpha, randomize=False, epsilon=None):
-        J = np.argmax(self.Z >= 1.0-alpha, axis=1).flatten()
+        L = np.argmax(self.Z >= 1.0-alpha, axis=1).flatten()
         if randomize:
             epsilon = np.random.uniform(low=0.0, high=1.0, size=self.n)
         if epsilon is not None:
-            Z_excess = np.array([ self.Z[i, J[i]] for i in range(self.n) ]) - (1.0-alpha)
-            p_remove = Z_excess / np.array([ self.prob_sort[i, J[i]] for i in range(self.n) ])
+            Z_excess = np.array([ self.Z[i, L[i]] for i in range(self.n) ]) - (1.0-alpha)
+            p_remove = Z_excess / np.array([ self.prob_sort[i, L[i]] for i in range(self.n) ])
             remove = epsilon <= p_remove
-            #pdb.set_trace()
             for i in np.where(remove)[0]:
-                J[i] = J[i] - 1
+                L[i] = L[i] - 1
         # Return prediction set
-        S = [ self.order[i,np.arange(0, J[i]+1)] for i in range(self.n) ]
+        S = [ self.order[i,np.arange(0, L[i]+1)] for i in range(self.n) ]
         return(S)
 
     def calibrate_scores(self, Y, epsilon=None):
@@ -33,8 +32,9 @@ class ProbabilityAccumulator:
         prob_cum = np.array([ self.Z[i,ranks[i]] for i in range(n2) ])
         prob = np.array([ self.prob_sort[i,ranks[i]] for i in range(n2) ])
         alpha_max = 1.0 - prob_cum
-        #if randomize: 
         if epsilon is not None:
-           alpha_max += np.multiply(prob, epsilon)
+            alpha_max += np.multiply(prob, epsilon)
+        else:
+            alpha_max += prob
         alpha_max = np.minimum(alpha_max, 1)
         return alpha_max
